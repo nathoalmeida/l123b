3559e851887ea40e841be4b976eba06a8ba48a0b
nathoalmeida <nathalia.oalmeida@gmail.com>
2023-09-07 16:04:24 -0300

    primeira correção

--- e4-nathalia.c
+++ e4-nathalia.c
@@ -9,10 +9,10 @@
 void imprime_digito(int digito);
 
 // função para converter os dígitos em hexadecimal
-void converter_hexadecimal(int valor); 
+void converter_hexadecimal(int valor);
 
 // função para converter os dígitos em binário
-void converter_binario(int valor); 
+void converter_binario(int valor);
 
 // função para converter os dígitos em octal
 void converter_octal(int valor);
@@ -23,135 +23,130 @@ void converter_decimal(int valor);
 // função que seleciona qual conversao será feita
 void selecionar_valor(int valor, int sis);
 
-
-void imprime_digito(int digito) {
-	if (digito <= 9) {
-	putchar(digito + '0');
- 
-	} else {
-  	putchar(digito - 10 + 'A');
-  }
-  } 
-
-void converter_hexadecimal(int valor) {
-	
-	int d1 = valor / 16;
-	int d2 = valor % 16;
-	
-	imprime_digito(d1);
-	imprime_digito(d2);
-} 
-
-void converter_binario(int valor) {
-	
-	int d1 = (valor / 2) / 2 / 2 / 2 / 2 / 2 / 2 % 2;
-	int d2 = (valor / 2) / 2 / 2 / 2 / 2 / 2 % 2;
-	int d3 = (valor / 2) / 2 / 2 / 2 / 2 % 2;
-	int d4 = (valor / 2) / 2 / 2 / 2  % 2;
-	int d5 = (valor / 2) / 2 / 2  % 2;
-	int d6 = (valor / 2) / 2  % 2;
-	int d7 = (valor / 2) % 2;
-	int d8 = valor % 2; 
-
-	imprime_digito(d1);
-	imprime_digito(d2);
-	imprime_digito(d3);
-	imprime_digito(d4);
-	imprime_digito(d5);
-	imprime_digito(d6);
-	imprime_digito(d7);
-	imprime_digito(d8);
-	
-} 
-
-void converter_octal(int valor) {
-	
-	int d1, d2, d3;
-	
-	
-	if(valor <= 255 && valor > 64) {
-		d1 = valor / 8 / 8;
-		d2 = valor / 8 % 8;
-		d3 = valor % 8;
-	} else { 
-		d1 = valor / 8 / 8;
-		d2 = valor / 8;
-		d3 = valor % 8;
-	}
-	
-	imprime_digito(d1);
-	imprime_digito(d2);
-	imprime_digito(d3);
-	
-} 
-
-void converter_decimal(int valor) {
-	int d1, d2, d3;
-	
-if(valor >= 0 && valor < 10) {
-	d1 = 0;
-	d2 = 0;
-	d3 = valor;
-} else if (valor >= 10 && valor <= 99) {
-	
-	d1 = 0;
-	d2 = valor / 10;
-	d3 = valor % 10;
-	
-} else {
-	d1 = valor / 100;
-	d2 = (valor - d1 * 100) / 10;
-	d3 = (valor - d1*100 - d2*10);
+void imprime_digito(int digito)
+{
+if (digito <= 9)
+{
+putchar(digito + '0');
 }
-	
-	imprime_digito(d1);
-	imprime_digito(d2);
-	imprime_digito(d3);
-	
-} 
+else
+{
+putchar(digito - 10 + 'A');
+}
 
- void selecionar_valor(int valor, int sis) {
-	
-	if (sis == 'h') {
-		converter_hexadecimal(valor);
-	}
-	else if (sis == 'b') {
-		converter_binario(valor);
-	}
-	else if (sis == 'd') {
-	    converter_decimal(valor);
-	}
-	else if (sis == 'o') {
-	    converter_octal(valor);
-	}
-	
-} 
+}
+
+void converter_hexadecimal(int valor)
+{
+
+    int d1 = valor / 16;
+    int d2 = valor % 16;
+
+    imprime_digito(d1);
+    imprime_digito(d2);
+}
+
+void converter_binario(int valor)
+{
+
+    int d1 = (valor / 2) / 2 / 2 / 2 / 2 / 2 / 2 % 2;
+    int d2 = (valor / 2) / 2 / 2 / 2 / 2 / 2 % 2;
+    int d3 = (valor / 2) / 2 / 2 / 2 / 2 % 2;
+    int d4 = (valor / 2) / 2 / 2 / 2 % 2;
+    int d5 = (valor / 2) / 2 / 2 % 2;
+    int d6 = (valor / 2) / 2 % 2;
+    int d7 = (valor / 2) % 2;
+    int d8 = valor % 2;
+
+    imprime_digito(d1);
+    imprime_digito(d2);
+    imprime_digito(d3);
+    imprime_digito(d4);
+    imprime_digito(d5);
+    imprime_digito(d6);
+    imprime_digito(d7);
+    imprime_digito(d8);
+}
+
+void converter_octal(int valor)
+{
+
+    int d1, d2, d3;
+
+
+    d1 = valor / 8 / 8;
+    d2 = valor / 8;
+    d3 = valor % 8;
+  
+
+    imprime_digito(d1);
+    imprime_digito(d2);
+    imprime_digito(d3);
+}
+
+void converter_decimal(int valor)
+{
+    int d1, d2, d3;
+
+    d1 = valor / 100;
+    d2 = (valor - d1 * 100) / 10;
+    d3 = (valor - d1 * 100 - d2 * 10);
+    
+
+    imprime_digito(d1);
+    imprime_digito(d2);
+    imprime_digito(d3);
+}
+
+void selecionar_valor(int valor, int sis)
+{
+
+    if (sis == 'h')
+    {
+        converter_hexadecimal(valor);
+    }
+    else if (sis == 'b')
+    {
+        converter_binario(valor);
+    }
+    else if (sis == 'd')
+    {
+        converter_decimal(valor);
+    }
+    else if (sis == 'o')
+    {
+        converter_octal(valor);
+    }
+}
 
 int main()
 {
 
     int numero;
 
-	scanf("%d", &numero);
-    
-    if (numero < 0) {
-		printf("Muito pequeno");
-	} else if (numero > 255) {
-		printf("Muito grande");
-	} else {
-		
-		printf("Em binario   octal   decimal   hexa\n");
-		printf("  ");
-		selecionar_valor(numero, 'b');
-		printf("     ");
-		selecionar_valor(numero, 'o');
-		printf("       ");
-		selecionar_valor(numero, 'd');
-		printf("     ");
-		selecionar_valor(numero, 'h');
-		
-	}
-	
+    scanf("%d", &numero);
+
+    if (numero < 0)
+    {
+        printf("Muito pequeno");
+    }
+    else if (numero > 255)
+    {
+        printf("Muito grande");
+    }
+    else
+    {
+
+        printf("Em binario   octal   decimal   hexa\n");
+        printf("  ");
+        selecionar_valor(numero, 'b');
+        printf("     ");
+        selecionar_valor(numero, 'o');
+        printf("       ");
+        selecionar_valor(numero, 'd');
+        printf("     ");
+        selecionar_valor(numero, 'h');
+    }
+
     return 0;
-    
 }
